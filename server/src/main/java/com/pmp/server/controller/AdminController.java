63baabc1c6bbac1d85008ad6485a2c2e8f86bd87
Thuong Nguyen <mttbit@gmail.com>
2022-05-13 11:00:14 -0500

    Admin ctr

--- server/src/main/java/com/pmp/server/controller/AdminController.java
+++ server/src/main/java/com/pmp/server/controller/AdminController.java
@@ -4,34 +4,29 @@ import com.google.common.base.CaseFormat;
 import com.pmp.server.domain.Role;
 import com.pmp.server.domain.User;
 import com.pmp.server.dto.APIResponse;
-import com.pmp.server.dto.common.PagingRequest;
 import com.pmp.server.service.RoleService;
 import com.pmp.server.service.UserService;
 import com.pmp.server.utils.enums.ERole;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/landlords")
-public class LandlordController {
+@RequestMapping("api/admin")
+public class AdminController {
 
     private final UserService userService;
     private final RoleService roleService;
 
-    public LandlordController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping
+    @GetMapping("/landlords")
     public APIResponse<User> getLandlords(Pageable pagingRequest, @RequestParam(required = false) String keywords) {
         Role role = roleService.findByName(ERole.ROLE_LANDLORD.getRole());
         PageRequest daoPageable = PageRequest.of(
@@ -41,6 +36,20 @@ public class LandlordController {
         );
 
         var data = userService.getAllByRoleIdAndKeywords(daoPageable, role, keywords != null ? keywords : "");
+        return new APIResponse<>(data);
+    }
+
+    @GetMapping("/tenants")
+    public APIResponse<User> getTenants(Pageable pagingRequest, @RequestParam(required = false) String keywords) {
+        Role role = roleService.findByName(ERole.ROLE_TENANT.getRole());
+
+        PageRequest daoPageable = PageRequest.of(
+                pagingRequest.getPageNumber(),
+                pagingRequest.getPageSize(),
+                convertDtoSortToDaoSort(pagingRequest.getSort())
+        );
+
+        var data = userService.getAllByRoleIdAndKeywords(daoPageable, role, keywords);
         return new APIResponse<>(data);
     }
 
