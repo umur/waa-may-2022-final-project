6f88baecb07a8fed8cdfa0f9b62d617e60e07c31
Thuong Nguyen <mttbit@gmail.com>
2022-05-13 14:31:05 -0500

    Split admin and client code

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,6 @@ import List from "./pages/List";
 import AuthWrapper from "./auth/AuthWrapper";
 import ROLE from "./auth/Role";
 import PropertyDetail from "./pages/PropertyDetail";
-import Dashboard from "./pages/admin/Dashboard";
-import Tenants from "./pages/admin/Tenant/Tenants";
-import NewTenant from "./pages/admin/Tenant/NewTenant";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme";
-import Tenant from './pages/admin/Tenant/Tenant';
-import Landlords from './pages/admin/Landlord/Landlords';
 
 function App() {
   const [isSignedIn, setSignedIn] = useState(
@@ -30,7 +23,6 @@ function App() {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
       <AuthContext.Provider value={authContext}>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -43,65 +35,8 @@ function App() {
               </AuthWrapper>
             }
           />
-          <Route
-            path="/admin"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <Dashboard />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/tenants"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <Tenants />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/tenants/:id"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <Tenant />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/tenants/new"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <NewTenant />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/landlords"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <Landlords />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/landlords/:id"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <Tenant />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/admin/landlords/new"
-            element={
-              <AuthWrapper role={[ROLE.Admin]}>
-                <NewTenant />
-              </AuthWrapper>
-            }
-          />
         </Routes>
       </AuthContext.Provider>
-    </ThemeProvider>
   );
 }
 
--- client/src/common/utils/hooks/use-calc-size.js
+++
@@ -1,6 +0,0 @@
-//size can be width, height, padding, margin, ...
-export const useCalcSize = (size, screenDesign) => {
-  const currentSize = (size / screenDesign) * 100;
-
-  return Math.round(currentSize);
-};
