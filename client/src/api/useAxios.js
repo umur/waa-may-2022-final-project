e9eff1d3718c1c37358118c4acd5fd59462e6bb9
Sai Kaung Khant <kaungkaung.m@gmail.com>
2022-05-13 11:31:56 -0500

    update on backend property controller and updated useAxios hooks on frontend

--- client/src/api/useAxios.js
+++ client/src/api/useAxios.js
@@ -1,13 +1,29 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import "firebase/database";
 import { AuthContext } from "../context/AuthContext";
 
-const useAxios = async (method, url, data = null) => {
+export const useAxios = (method, url, postData = null) => {
   const { isSignedIn } = useContext(AuthContext);
-  let response = await axios[method](process.env.API_URL + url, { data });
-  let result = await response.json();
-  return result;
-};
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-export default useAxios;
+  const executeRequest = useCallback(async () => {
+    try {
+      let response = await axios[method](url);
+      setData(response.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [method, url]);
+
+  useEffect(() => {
+    if (data === null && method === "get") {
+      executeRequest();
+    }
+  }, [data, executeRequest, method]);
+
+  return { data, error, loading, execute: executeRequest };
+};
--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+axios.defaults.baseURL = "http://localhost:8080/api/";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
