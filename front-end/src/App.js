2c6e44370105c0548b3e7c45508b016e03e3d4e4
vivekshubhu <bibek.karki001@gmail.com>
2022-05-13 23:33:45 -0500

    creates landlord list page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,27 +4,36 @@ import Signup from './pages/Signup';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './components/Dashboard';
+import LandLordList from './pages/landlord/LandLordList';
 
 function App() {
   return (
-   <>
-    <Routes>
-      <Route path="/" element={<Navigate to="/dashboard" />}></Route>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />}>
 
-      <Route
-       path='/dashboard'
-       element={
-        <PrivateRoute path = '/login'><Home /></PrivateRoute>
-        }>
-      </Route>
+        </Route>
 
-      <Route path='/login' element={<Login/>}>
-      </Route>
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Home></Home>
+            </PrivateRoute>
+          }>
+          <Route path='' element={<Dashboard />}>
+          </Route>
+          <Route path='landlords' element={<LandLordList />}></Route>
+        </Route>
 
-      <Route path='/signup' element={<Signup/>}>
-      </Route>
-    </Routes>
-   </>
+        <Route path='/login' element={<Login />}>
+        </Route>
+
+        <Route path='/signup' element={<Signup />}>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
