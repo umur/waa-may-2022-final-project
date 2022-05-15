5ad209099ec047050e058909762d18ea318b356b
Sai Kaung Khant <kaungkaung.m@gmail.com>
2022-05-15 17:57:26 -0500

    admin panel edit page and detail page update

--- admin/src/pages/Tenant/Tenant.js
+++ admin/src/pages/Tenant/Tenant.js
@@ -1,18 +1,38 @@
-import faker from '@faker-js/faker';
-import { Box, Divider, Grid, Paper, Typography } from '@mui/material';
-import StatusView, { StatusVariants } from 'components/StatusView';
-import Layout from 'pages/Layout';
-import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useAxios } from '../../api/userAxios';
-import { ToastContainer, toast } from 'react-toastify';
+import faker from "@faker-js/faker";
+import {
+  Box,
+  Divider,
+  Grid,
+  Paper,
+  Typography,
+  Modal,
+  Button,
+  TextField,
+} from "@mui/material";
+import StatusView, { StatusVariants } from "components/StatusView";
+import Layout from "pages/Layout";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAxios } from "../../api/userAxios";
+import { ToastContainer, toast } from "react-toastify";
+import EditIcon from "@mui/icons-material/Edit";
+import Loading from "../../components/Loading";
+import Header from "../../components/Header";
 
-import Loading from '../../components/Loading';
-import Header from '../../components/Header';
-
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Tenant = () => {
   const { id } = useParams();
+  const [open, setOpen] = useState(false);
   const notify = (msg) => toast.error(msg);
 
   const { data, error, loading, execute, queryParam } = useAxios(
@@ -29,17 +49,37 @@ const Tenant = () => {
     );
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    console.log({
+      data: { email: form.get("email"), password: form.get("password") },
+    });
+  };
 
   return (
     <Layout title="Tenant">
       <ToastContainer />
       <Paper>
-        <Grid container direction="row" mt={2} p={2} spacing={2}>
+        <div style={{ padding: "20px 20px 0px 20px" }}>
+          <Typography component="h1" variant="h5">
+            Tenant Infromation
+          </Typography>
+        </div>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          style={{
+            margin: "0px 20px 20px 20px",
+            padding: "20px",
+          }}
+        >
           <Grid item xs={4}>
-            <Typography>
-              First Name: {data.data.firstName}
-            </Typography>
-
+            <Typography>First Name: {data.data.firstName}</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>First Name: {data.data.firstName}</Typography>
           </Grid>
           {/* <Grid item xs={4}>
             <StatusView title={tenant.status.toUpperCase()} variant={tenant.status} />
@@ -52,9 +92,47 @@ const Tenant = () => {
             <StatusView title={tenant.status.toUpperCase()} variant={tenant.status} />
           </Grid> */}
         </Grid>
+        <div className="edit-btn">
+          <Button onClick={() => setOpen(!open)} variant="contained">
+            <EditIcon /> Edit
+          </Button>
+        </div>
       </Paper>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} component="form" onSubmit={handleSubmit} noValidate>
+          <Typography component="h1" variant="h5">
+            Update
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              size="small"
+            />
+            <div className="btn-list">
+              <Button type="submit" sx={{ mt: 3, mb: 2 }}>
+                Update
+              </Button>
+              <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+                Update
+              </Button>
+            </div>
+          </Box>
+        </Box>
+      </Modal>
     </Layout>
   );
-}
+};
 
 export default Tenant;
