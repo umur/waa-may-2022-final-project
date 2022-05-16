37031ef87c1021ae9484770ae86483bade6d1845
jairnb <jair10.nb@hotmail.com>
2022-05-16 17:52:53 -0500

    property

--- frontend/waafront/src/pages/dashbord/dashboard-admin.js
+++ frontend/waafront/src/pages/dashbord/dashboard-admin.js
@@ -4,9 +4,14 @@ import { useSelector } from 'react-redux'
 
 const DashboardAdmin = () => {
 
-  const userToken = useSelector((state) => state.user.value);
+  const user = useSelector((state) => state.user.value); //token, role, email, id
 
+  const config = {
+    headers: { Authorization: `Bearer ${user.token}` }
+  };
 
+  
+  console.log(user);
 
   return (
     <div>dashboard-admin</div>
