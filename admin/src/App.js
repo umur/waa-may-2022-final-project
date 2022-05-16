66993cac9718115b845693ea5852fe1937783c7b
Thuong Nguyen <mttbit@gmail.com>
2022-05-15 19:05:34 -0500

    Add 404 page

--- admin/src/App.js
+++ admin/src/App.js
@@ -16,6 +16,7 @@ import Register from "pages/Register";
 import ForgotPassword from "pages/ForgotPassword";
 import Properties from "pages/Properties";
 import { SignalWifiStatusbarNullSharp } from "@mui/icons-material";
+import NotFound from 'pages/404';
 
 function App() {
   const [isSignedIn, setSignedIn] = useState(
@@ -44,6 +45,8 @@ function App() {
     <ThemeProvider theme={theme}>
       <AuthContext.Provider value={authContext}>
         <Routes>
+          <Route path="*" element={<NotFound />} />
+
           <Route path="/login" element={<Login />} />
           {/* <Route path="/register" element={<Register />} /> */}
           <Route path="/forgotpassword" element={<ForgotPassword />} />
