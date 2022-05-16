3aaebece418f3a14454fb732f085115b0961075a
Gregory Ndukwu <ndukwusud@gmail.com>
2022-05-15 22:30:01 -0500

    Front Gregory

--- frontend/waafront/src/pages/properties/index.js
+++ frontend/waafront/src/pages/properties/index.js
@@ -1,188 +1,133 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import PropertiesTable from './propertiesTable';
+import PropertiesTable from "./propertiesTable";
 
-import { Modal, Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Modal, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import FormSizeDemo from "./formPropertieStyled";
 
 import {
-    Form,
-    Input,
-    Radio,
-    Select,
-    Cascader,
-    DatePicker,
-    InputNumber,
-    TreeSelect,
-    Switch,
-} from 'antd';
+  Form,
+  Input,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from "antd";
 
 function Properties() {
+  const [size, setSize] = useState("large");
 
-    const [size, setSize] = useState('large');
+  const [propertyState, setPropertyState] = useState([
+    {
+      id: 0,
+      name: "",
+      street: "",
+      city: "",
+      state: "",
+      zip: "",
+      numberOfBedrooms: 0,
+      numberOfBathrooms: 0,
+      rentAmount: 25,
+      securityDepositAmount: 54,
+      occupied: false,
+      listed: true,
+      photos: [],
+      propertyType: {
+        id: 1,
+        name: "Single-Family Homes",
+      },
+      user: {
+        id: 1,
+        email: "admin@admin.com",
+        firstname: "John",
+        lastname: "Doe",
+        active: true,
+        role: "ADMIN",
+        lastLoggedInAt: null,
+      },
+      rent: [],
+    },
+  ]);
 
-    const [propertyState, setPropertyState] = useState([
-        {
-            id: 0,
-            name: "",
-            street: "",
-            city: "",
-            state: "",
-            zip: "",
-            numberOfBedrooms: 0,
-            numberOfBathrooms: 0,
-            rentAmount: 25,
-            securityDepositAmount: 54,
-            occupied: false,
-            listed: true,
-            photos: [],
-            propertyType: {
-                id: 1,
-                name: "Single-Family Homes"
-            },
-            user: {
-                id: 1,
-                email: "admin@admin.com",
-                firstname: "John",
-                lastname: "Doe",
-                active: true,
-                role: "ADMIN",
-                lastLoggedInAt: null
-            },
-            rent: []
-        }
+  const fetchProducts = async () => {
+    const result = await axios.get("http://localhost:8080/api/v1/properties");
+    setPropertyState(result.data);
+  };
 
-    ]);
+  useEffect(() => {
+    fetchProducts();
+    forceUpdate({});
+  }, []);
 
-    const fetchProducts = async () => {
-        const result = await axios.get('http://localhost:8080/api/v1/properties')
-        setPropertyState(result.data)
-    }
+  const showModal = () => {
+    setVisible(true);
+  };
 
-    useEffect(() => {
-        fetchProducts();
-        forceUpdate({});
-    }, [])
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
 
-    const showModal = () => {
-        setVisible(true);
-    };
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
 
-    const handleOk = () => {
-        setModalText('The modal will be closed after two seconds');
-        setConfirmLoading(true);
-        setTimeout(() => {
-            setVisible(false);
-            setConfirmLoading(false);
-        }, 2000);
-    };
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [modalText, setModalText] = React.useState("Content of the modal");
 
-    const handleCancel = () => {
-        console.log('Clicked cancel button');
-        setVisible(false);
-    };
+  const [componentSize, setComponentSize] = useState("default");
 
-    const [visible, setVisible] = React.useState(false);
-    const [confirmLoading, setConfirmLoading] = React.useState(false);
-    const [modalText, setModalText] = React.useState('Content of the modal');
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
 
-    const [componentSize, setComponentSize] = useState('default');
+  const onFinish = (values) => {
+    console.log("Finish:", values);
+  };
 
-    const onFormLayoutChange = ({ size }) => {
-        setComponentSize(size);
-    };
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState({});
 
-    const onFinish = (values) => {
-        console.log('Finish:', values);
-    };
+  return (
+    <>
+      <div>
+        <Button
+          onClick={showModal}
+          type="primary"
+          icon={<PlusOutlined />}
+          size={size}
+          style={{ float: "right", marginBottom: 20 }}
+        >
+          Add Property
+        </Button>
+      </div>
 
-    const [form] = Form.useForm();
-    const [, forceUpdate] = useState({});
+      <PropertiesTable propertyList={propertyState}></PropertiesTable>
 
-    return (
-        <>
-            <div>
-                <Button onClick={showModal} type="primary" icon={<PlusOutlined />} size={size} style={{ float: 'right', marginBottom: 20 }}>
-                    Add Property
-                </Button>
-            </div>
-
-            <PropertiesTable propertyList={propertyState} ></PropertiesTable>
-
-
-
-
-            <Modal
-                title="Title"
-                visible={visible}
-                onOk={handleOk}
-                confirmLoading={confirmLoading}
-                onCancel={handleCancel}
-                width={900}
-            >
-                <Form
-                    labelCol={{
-                        span: 4,
-                    }}
-                    wrapperCol={{
-                        span: 14,
-                    }}
-                    layout="inline"
-                    initialValues={{
-                        size: componentSize,
-                    }}
-                    onValuesChange={onFormLayoutChange}
-                    size={componentSize}
-
-                >
-
-                    <Form.Item
-                        name="username"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your username!',
-                            },
-                        ]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your password!',
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <br/>
-                    <Form.Item shouldUpdate>
-                        {() => (
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                disabled={
-                                    !form.isFieldsTouched(true) ||
-                                    !!form.getFieldsError().filter(({ errors }) => errors.length).length
-                                }
-                            >
-                                Log in
-                            </Button>
-                        )}
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
-    )
+      <Modal
+        title="Title"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        width={900}
+      >
+        <FormSizeDemo></FormSizeDemo>
+      </Modal>
+    </>
+  );
 }
 
 export default Properties;
