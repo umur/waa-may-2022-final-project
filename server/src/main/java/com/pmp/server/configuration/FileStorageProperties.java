0b5add6ce46037d581df4b8462013acb5e9fefeb
Thuong Nguyen <mttbit@gmail.com>
2022-05-16 09:27:29 -0500

    Finish serving image for public access

--- server/src/main/java/com/pmp/server/configuration/FileStorageProperties.java
+++ server/src/main/java/com/pmp/server/configuration/FileStorageProperties.java
@@ -2,12 +2,12 @@ package com.pmp.server.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "file")
+//@ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
     private String uploadDir;
 
     public String getUploadDir() {
-        return uploadDir;
+        return System.getProperty("user.dir") + "/target/static";
     }
 
     public void setUploadDir(String uploadDir) {
