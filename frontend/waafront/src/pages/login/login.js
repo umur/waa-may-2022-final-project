d508301d1f9156baa539e623ea725557d6c90762
Gregory Ndukwu <ndukwusud@gmail.com>
2022-05-17 02:29:36 -0500

    Greg front

--- frontend/waafront/src/pages/login/login.js
+++ frontend/waafront/src/pages/login/login.js
@@ -1,103 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import api from '../../api/posts'
-import { useDispatch } from 'react-redux';
-import { login } from '../../features/user-slice';
+import api from "../../api/posts";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/user-slice";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [res, setRes] = useState('');
-    let navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [res, setRes] = useState("");
+  let navigate = useNavigate();
 
-    const dispath = useDispatch();
+  const dispath = useDispatch();
 
-    const sendInfo = async () => {
-        const credentials = { email: email, password: password };
+  const sendInfo = async () => {
+    const credentials = { email: email, password: password };
 
-        try{
+    try {
+      const response = await api.post("api/v1/login", credentials);
+      setRes(response.data);
 
-            const response = await api.post('api/v1/login', credentials);
-            setRes(response.data);
-    
-            const theTokenString = JSON.stringify(response.data) ;
-            const theToken=JSON.parse(theTokenString).jwtToken;
-            console.log(theToken);
-            
-            const decodedString = atob(theToken.split('.')[1]);
-            const userRole = JSON.parse(decodedString).role[0].authority;
-            const id=JSON.parse(decodedString).id;
-            const userName = JSON.parse(decodedString).sub;
-    
-            window.sessionStorage.setItem("userRole", userRole);
-            window.sessionStorage.setItem("userName", userName);
-            window.sessionStorage.setItem("token", theToken);
-            window.sessionStorage.setItem("id", id);
+      const theTokenString = JSON.stringify(response.data);
+      const theToken = JSON.parse(theTokenString).jwtToken;
+      console.log(theToken);
 
-            if(userRole==='ADMIN'){
-                navigate(`/dashboard-admin`);
-            }else if(userRole==='TENANT'){
-                navigate(`/dashboard-tenant`);
-            }else if(userRole==='LANDLORD'){
-                navigate(`/dashboard-landlord`);
-            }
-            
-    
-            dispath(login({ role: userRole, email: userName, token:theToken, id:id }));
-        }catch (err){
-            if(err.response){
-                alert ('Login failed');
-            }else{
-                alert ('Login failed');
-            }
-        }
-       
-       
+      const decodedString = atob(theToken.split(".")[1]);
+      const userRole = JSON.parse(decodedString).role[0].authority;
+      const id = JSON.parse(decodedString).id;
+      const userName = JSON.parse(decodedString).sub;
+
+      window.sessionStorage.setItem("userRole", userRole);
+      window.sessionStorage.setItem("userName", userName);
+      window.sessionStorage.setItem("token", theToken);
+      window.sessionStorage.setItem("id", id);
+
+      if (userRole === "ADMIN") {
+        navigate(`/dashboard-admin`);
+      } else if (userRole === "TENANT") {
+        navigate(`/dashboard-tenant`);
+      } else if (userRole === "LANDLORD") {
+        navigate(`/dashboard-landlord`);
+      }
+
+      dispath(
+        login({ role: userRole, email: userName, token: theToken, id: id })
+      );
+    } catch (err) {
+      if (err.response) {
+        alert("Login failed");
+      } else {
+        alert("Login failed");
+      }
     }
+  };
 
-    return (
-        <div className='row'>
-            <div className='col-sm-4'>
-
+  return (
+    <div className="row">
+      <div className="col-sm-4"></div>
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-header">Login</div>
+          <div className="card-body">
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control form-control-sm"
+              ></input>
             </div>
-            <div className='col-sm-4'>
-                <div className="card">
-                    <div className="card-header">
-                        Login
-                    </div>
-                    <div className="card-body">
-                        <div className='form-group'>
-                            <label>Eail</label>
-                            <input
-                                type='text'
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className='form-control form-control-sm'>
-                            </input>
-                        </div>
-                        <br></br>
-                        <div className='form-group'>
-                            <label>Password</label>
-                            <input
-                                type='password'
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className='form-control form-control-sm'>
-                            </input>
-                        </div>
-                        <br></br>
-                        <div className='form-group'>
-                            <button className='btn btn-sm btn-primary' onClick={sendInfo}>Login</button>
-                            <Link to='/forgot-password'>Forgot password? Reset from here.</Link>
-                        </div>
-                    </div>
-                </div>
+            <br></br>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control form-control-sm"
+              ></input>
             </div>
-            <div className='col-sm-4'>
-
+            <br></br>
+            <div className="form-group">
+              <button className="btn btn-sm btn-primary" onClick={sendInfo}>
+                Login
+              </button>
+              <Link to="/forgot-password">
+                Forgot password? Reset from here.
+              </Link>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="col-sm-4"></div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/waafront/src/pages/properties/propertiesTable.js
+++ frontend/waafront/src/pages/properties/propertiesTable.js
@@ -1,84 +1,241 @@
-import { Table, Tag, Space } from "antd";
 import "antd/dist/antd.css";
-import { Button } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import axios from "axios";
 import React, { useState } from "react";
-import Properties from ".";
-import updatePropertyForm from "./updatePropertie";
-import UpdateProperty from "./updatePropertie";
+import { Table, Tag, Space, Switch } from "antd";
+import "antd/dist/antd.css";
+import { Button, Modal } from "antd";
+import { Form, Input, InputNumber } from "antd";
 
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
+let propertyObjectSave = {
+  id: 0,
+  name: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  numberOfBedrooms: 0,
+  numberOfBathrooms: 0,
+  rentAmount: 25,
+  securityDepositAmount: 54,
+  occupied: false,
+  listed: true,
+  photos: [],
+  propertyType: {
+    id: 1,
+    name: "Single-Family Homes",
   },
-  {
-    title: "Street",
-    dataIndex: "street",
-    key: "street",
+  user: {
+    id: 1,
+    email: "admin@admin.com",
+    firstname: "John",
+    lastname: "Doe",
+    active: true,
+    role: "ADMIN",
+    lastLoggedInAt: null,
   },
-  {
-    title: "City",
-    dataIndex: "city",
-    key: "city",
-  },
-  {
-    title: "Type",
-    key: "propertyType",
-    dataIndex: "propertyType",
-    render: (propertyType) => <span>{propertyType.name}</span>,
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (text, record) => (
-      <Space size="middle">
-        <Button
-          onClick={() => <UpdateProperty id={record.id} />}
-          type="primary"
-          icon={<EditOutlined />}
-        ></Button>
-        <Button
-          onClick={() => {
-            deleteProperty(record);
-          }}
-          type="danger"
-          icon={<DeleteOutlined />}
-        ></Button>
-      </Space>
-    ),
-  },
-];
-
-const deleteProperty = async (property) => {
-  const id = property.id;
-  try {
-    const { data } = await axios.delete(
-      `http://localhost:8080/api/v1/properties/${id}`
-    );
-    window.alert("The Properties  was Deleted");
-    window.location.replace("http://localhost:3000/properties");
-  } catch (e) {}
+  rent: [],
 };
+function PropertyTable(props) {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Street",
+      dataIndex: "street",
+      key: "street",
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city",
+    },
+    {
+      title: "Type",
+      key: "propertyType",
+      dataIndex: "propertyType",
+      render: (propertyType) => <span>{propertyType.name}</span>,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Button
+            onClick={() => handleEdit(record)}
+            type="primary"
+            icon={<EditOutlined />}
+          ></Button>
+          <Button
+            onClick={() => {
+              deleteProperty(record);
+            }}
+            type="danger"
+            icon={<DeleteOutlined />}
+          ></Button>
+        </Space>
+      ),
+    },
+  ];
 
-const updateProperty = async (property) => {
-  const idPro = property.id;
-  UpdateProperty(idPro);
-  // console.log(idPro);
-  // try {
-  //   const { data } = await axios.get(
-  //     `http://localhost:8080/api/v1/properties/${idPro}`
-  //   );
-  //   console.log(data);
-  //   return <Properties propertiesObject={property} />;
-  // } catch (e) {}
-};
+  const handleEdit = (param) => {
+    setPropertyState(param);
+    console.log(param);
+    setVisible(true);
+  };
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [propertyState, setPropertyState] = useState(propertyObjectSave);
 
-export default (props) => (
-  <Table columns={columns} dataSource={props.propertyList} rowKey="id" />
-);
+  const onFieldsChanged = (event) => {
+    //alert("I am here");
+    let copy = { ...propertyState };
+    //console.log(copy);
+    copy[event.target.name] = event.target.value;
+    setPropertyState(copy);
+  };
 
-const showFormUpdate = () => {};
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
+
+  const handleOk = async () => {
+    try {
+      const { data } = await axios.put(
+        "http://localhost:8080/api/v1/properties/" + propertyState.id,
+        propertyState
+      );
+      window.alert("Property updated");
+      window.location.replace("http://localhost:3000/properties");
+      setVisible(false);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const deleteProperty = async (property) => {
+    const id = property.id;
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:8080/api/v1/properties/${id}`
+      );
+      window.alert("The Properties  was Deleted");
+      window.location.replace("http://localhost:3000/properties");
+    } catch (e) {}
+  };
+
+  return (
+    <>
+      <Table columns={columns} dataSource={props.propertyList} rowKey="id" />
+      <Modal
+        title="Update Property Informations"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        width={900}
+      >
+        <Form>
+          <Form.Item label="Name">
+            <Input
+              name="name"
+              value={propertyState.name}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+          <Form.Item label="Street">
+            <Input
+              name="street"
+              value={propertyState.street}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+          <Form.Item label="City">
+            <Input
+              name="city"
+              value={propertyState.city}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+          <Form.Item label="States">
+            <Input
+              name="state"
+              maxLength={2}
+              style={{ width: "7%" }}
+              value={propertyState.state}
+              onChange={onFieldsChanged}
+            />
+            {/* <Select
+              fieldNames={{
+                value: propertyState.state,
+                name: propertyState.state,
+              }}
+              onChange={onFieldsChanged}
+            >
+              <Select.Option value=""></Select.Option>
+              {statesOptions.map((statesOption) => (
+                <option value={statesOption.value}>{statesOption.title}</option>
+              ))}
+            </Select> */}
+          </Form.Item>
+          <Form.Item label="Zip Code">
+            <Input
+              name="zip"
+              style={{ width: "30%" }}
+              value={propertyState.zip}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+
+          <Form.Item label="Number of Bedrooms">
+            <InputNumber
+              name="numberOfBedrooms"
+              style={{ width: "30%" }}
+              value={propertyState.numberOfBedrooms}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+          <Form.Item label="Number of Bathrooms">
+            <InputNumber
+              name="numberOfBathrooms"
+              style={{ width: "30%" }}
+              value={propertyState.numberOfBathrooms}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+
+          <Form.Item label="Rent Amount">
+            <Input
+              name="rentAmount"
+              style={{ width: "30%" }}
+              value={propertyState.rentAmount}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+          <Form.Item label="Security Deposit">
+            <Input
+              name="securityDepositAmount"
+              style={{ width: "30%" }}
+              value={propertyState.securityDepositAmount}
+              onChange={onFieldsChanged}
+            />
+          </Form.Item>
+
+          <Form.Item label="Occupied" valuePropName="checked">
+            <Switch value={propertyState.occupied} onChange={onFieldsChanged} />
+          </Form.Item>
+          <Form.Item label="Listed" valuePropName="checked">
+            <Switch value={propertyState.listed} onChange={onFieldsChanged} />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+}
+
+export default PropertyTable;
