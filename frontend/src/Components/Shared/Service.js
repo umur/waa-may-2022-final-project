0365d47d5b414e112a9c0a906d58b44ba24797e9
abdelrahman89 <abdelrahman.freek@gmail.com>
2022-05-18 15:39:02 -0500

    update url and testing appliction

--- frontend/src/Components/Shared/Service.js
+++ frontend/src/Components/Shared/Service.js
@@ -1,24 +1,24 @@
 const Service = {
-  RentProperty: " http://172.17.12.80:8080/api/properties/tenant",
+  RentProperty: " http://localhost:8080/api/properties/tenant",
   TotalIncomePerLocation:
-    "http://172.17.12.80:8080/api/properties/admin/totalIncome/Fairfield",
+    "http://localhost:8080/api/properties/admin/totalIncome/Fairfield",
   MostRecentLandlordsUrl:
-    "http://172.17.12.80:8080/api/users/admin/byRole/landlord",
-  MostRecentTenantsUrl: "http://172.17.12.80:8080/api/users/admin/recentUsers",
-  Properties: "http://172.17.12.80:8080/api/properties",
+    "http://localhost:8080/api/users/admin/byRole/landlord",
+  MostRecentTenantsUrl: "http://localhost:8080/api/users/admin/recentUsers",
+  Properties: "http://localhost:8080/api/properties",
   LeasesInOneMonth:
-    "http://172.17.12.80:8080/api/properties/landlord/leases/john@gmail.com",
+    "http://localhost:8080/api/properties/landlord/leases/john@gmail.com",
   GetPropertiesOwnedBy:
-    "http://172.17.12.80:8080/api/properties/landlord/ownedBy/john@gmail.com",
-  GetPropertyType: "http://172.17.12.80:8080/api/properties/propertyTypes",
-  AddProperty: "http://172.17.12.80:8080/api/properties/landlord",
-  TenantGetAllProperties: "http://172.17.12.80:8080/api/properties",
-  TenantRentProperty: "http://172.17.12.80:8080/api/properties/tenant",
+    "http://localhost:8080/api/properties/landlord/ownedBy/john@gmail.com",
+  GetPropertyType: "http://localhost:8080/api/properties/propertyTypes",
+  AddProperty: "http://localhost:8080/api/properties/landlord",
+  TenantGetAllProperties: "http://localhost:8080/api/properties",
+  TenantRentProperty: "http://localhost:8080/api/properties/tenant",
   TenantFilterByCity:
-    "http://172.17.12.80:8080/api/properties/tenant/filterByCity/",
+    "http://localhost:8080/api/properties/tenant/filterByCity/",
   LandlordFilterByCity:
-    "http://172.17.12.80:8080/api/properties/landlord/ownedBy/john@gmail.com/Fairfield",
-  SignUp: "http://172.17.12.80:8080/api/users/signup"
+    "http://localhost:8080/api/properties/landlord/ownedBy/john@gmail.com/Fairfield",
+  SignUp: "http://localhost:8080/api/users/signup"
 };
 
 export default Service;
