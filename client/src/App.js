c4edeb40a0acd8d6fb38acb9d76681cb32e73dc7
MaherFayezk <92121234+MaherFayezk@users.noreply.github.com>
2022-05-18 12:51:56 -0500

    Implement the slide show and  finalize the tenant property page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Home from "./routes/Home";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
 import Properties from "./routes/properties";
 import { Route, Routes } from "react-router-dom";
 import Lease from "./routes/Lease";
 import PropertyRoute from "./routes/PropertyRouter";
+import Register from "./routes/Register";
+import Login from "./routes/Login";
 //redux or context(wrap the whole app with auth context)
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/properties" element={<Properties />} />
           <Route path="/properties/:id/lease" element={<Lease />} />
           <Route path="/properties/:id" element={<PropertyRoute />} />
