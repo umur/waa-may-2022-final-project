608fe30ef22bb40e14480fc982a64bdd95f23371
MaherFayezk <92121234+MaherFayezk@users.noreply.github.com>
2022-05-18 12:55:21 -0500

    remove static data

--- client/src/components/PropertyDetails.js
+++ client/src/components/PropertyDetails.js
@@ -2,14 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import TextMobileStepper from "../components/slideShow"
+import ListImages from "../components/slideShow";
 
 export const PropertyDetails = () => {
   const { id } = useParams();
   const [propertyState, setProertyState] = useState({});
 
   const fetchProerty = () => {
-    axios.get(`http://localhost:8081/properties/${id}`)
+    axios
+      .get(`http://localhost:8081/properties/${id}`)
       .then((response) => {
         setProertyState(response.data);
       })
@@ -20,13 +21,18 @@ export const PropertyDetails = () => {
 
   useEffect(() => {
     fetchProerty();
-  }, []);
+  });
 
   return (
     <div>
       <h1>{propertyState.name}</h1>
       <h1>{propertyState.description}</h1>
-      <TextMobileStepper />
+      <ListImages
+        images={[
+          "https://bartlebrothers.com/wp-content/uploads/2015/02/Condos-and-houses-in-San-Diego-California.jpg",
+          "https://www.goalproperties.com/wp-content/uploads/2019/07/3239-Runneymede-large.jpg",
+        ]}
+      />
     </div>
   );
 };
--- client/src/components/slideShow.js
+++ client/src/components/slideShow.js
@@ -3,7 +3,6 @@ import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MobileStepper from "@mui/material/MobileStepper";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
@@ -12,33 +11,10 @@ import { autoPlay } from "react-swipeable-views-utils";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const images = [
-  {
-    label: "San Francisco – Oakland Bay Bridge, United States",
-    imgPath:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bird",
-    imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bali, Indonesia",
-    imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80",
-  },
-  {
-    label: "Goč, Serbia",
-    imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-];
-
-function SwipeableTextMobileStepper() {
+function ListImages(props) {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const maxSteps = props.images.length;
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -48,45 +24,42 @@ function SwipeableTextMobileStepper() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleStepChange = (step: number) => {
+  const handleStepChange = (step) => {
     setActiveStep(step);
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+    <Box sx={{ maxWidth: 1000, flexGrow: 1 }}>
       <Paper
         square
         elevation={0}
         sx={{
-          display: "flex",
           alignItems: "center",
           height: 50,
           pl: 2,
           bgcolor: "background.default",
         }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
+      ></Paper>
       <AutoPlaySwipeableViews
         axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
         onChangeIndex={handleStepChange}
         enableMouseEvents
       >
-        {images.map((step, index) => (
-          <div key={step.label}>
+        {props.images.map((step, index) => (
+          <div key={step}>
             {Math.abs(activeStep - index) <= 2 ? (
               <Box
                 component="img"
                 sx={{
-                  height: 255,
+                  height: 600,
                   display: "block",
-                  maxWidth: 400,
+                  maxwidth: 600,
                   overflow: "hidden",
                   width: "100%",
+                  alignItems: "center",
                 }}
-                src={step.imgPath}
-                alt={step.label}
+                src={step}
               />
             ) : null}
           </div>
@@ -125,4 +98,4 @@ function SwipeableTextMobileStepper() {
   );
 }
 
-export default SwipeableTextMobileStepper;
+export default ListImages;
